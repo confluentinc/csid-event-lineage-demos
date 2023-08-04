2a095dff16ad0acd189f1a753860f4d370788f0c
Roman Kolesnev <rkolesnev@confluent.io>
2023-08-04 12:13:55 +0100

    move and rename extensions jar

--- run_demo.sh
+++ run_demo.sh
@@ -4,6 +4,7 @@ cd demo/javaapps
 ./gatherjars.sh
 curl https://repo1.maven.org/maven2/io/opentelemetry/javaagent/opentelemetry-javaagent/1.13.0/opentelemetry-javaagent-1.13.0.jar -o opentelemetry-javaagent-1.13.0.jar
 # TODO: automate download of extension
+cp ../../lineage-opentel-extensions-0.0.1-SNAPSHOT-all.jarfile lineage-opentel-extensions-0.0.1-SNAPSHOT-all.jar
 cd ..
 echo "Building the demo environment"
 docker-compose build javaapps
